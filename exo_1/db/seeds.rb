11eb89f4e13cf8034c36256cc0e4a95fa93b0273
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-04-25 10:23:18 +0200

    exo non finis

---
+++ exo_1/db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Movie.destroy_all
+
+100.times do |index|
+  Movie.create(
+  	name: Faker::DcComics.hero,
+  	year: Faker::Number.between(1900,2020),
+  	genre: [:action, :horreur, :comédie, :drame].sample,
+  	synopsis: Faker::Lorem.words(15),
+  	director: Faker::Name.name,
+  	allocine_rating: Faker::Number.between(0.0, 5.0).round(2),
+  	my_rating: nil,
+  	already_seen: false
+  	);
+end
